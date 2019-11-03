f624ef008d79520be57c69e93c774ff14d0daf4b
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2019-11-02 23:36:51 -0300

    rep of a ball

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import * as BABYLON from 'babylonjs';
 import 'babylonjs-loaders';
-import '@babylonjs/loaders';
+import './pool';
+import './pool';
+import AssetsImport from './pool';
+import Ball from './ball';
 
 const mainCanvas = document.querySelector("#mainRender");
 // console.log(mainCanvas);
@@ -9,20 +12,16 @@ const engine = new BABYLON.Engine(mainCanvas, true, {preserveDrawingBuffer: true
 
 
 const createScene = ()=>{
-
     const scene = new BABYLON.Scene(engine);
 
-    var model = new BABYLON.SceneLoader.AppendAsync('assets/models/Pool_table/','pool_table.obj', scene, (err, scene)=>{ 
-    });
-
+    const BALL_SYSTEM = new Ball();
+    console.log(BALL_SYSTEM.balls);
+    AssetsImport.importPool(scene);
     var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene);
+    BALL_SYSTEM.createBall(0,0, scene);
     camera.attachControl(mainCanvas, true);
-
     camera.setPosition(new BABYLON.Vector3(0, 80, 20));
-
-
-    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-    // Return the created scene
+    new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
     return scene;
 }
 
